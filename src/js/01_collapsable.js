98b73337605e0012e125fec1518393a587fa32df
Lourdesjupo <lourdes.jupo@gmail.com>
2023-05-26 14:07:11 +0200

    arrowUp

--- src/js/01_collapsable.js
+++ src/js/01_collapsable.js
@@ -4,6 +4,8 @@ const designUnfold = document.querySelector('.js_design-color__unfold');
 const designForm = document.querySelector('.js_color-selection');
 const fillUnfold = document.querySelector('.js_fill-color__unfold');
 const fillForm = document.querySelector('.js_fillform');
+const fillArrow = document.querySelector('.js_arrow_fill');
+const designArrow =document.querySelector('.js_arrow_design');
 
 // funciones
 function handleClickDesignUnfold(event) {
@@ -11,8 +13,11 @@ function handleClickDesignUnfold(event) {
   if (designForm.classList.contains('collapsed')) {
     designForm.classList.remove('collapsed');
     fillForm.classList.add('collapsed');
+    fillArrow.classList.remove('arrowUp');
+    designArrow.classList.add('arrowUp');
   } else {
     designForm.classList.add('collapsed');
+    designArrow.classList.remove('arrowUp');
   }
 }
 function handleClickFillUnfold(event) {
@@ -20,10 +25,19 @@ function handleClickFillUnfold(event) {
   if (fillForm.classList.contains('collapsed')) {
     fillForm.classList.remove('collapsed');
     designForm.classList.add('collapsed');
+    designArrow.classList.remove('arrowUp');
+    fillArrow.classList.add('arrowUp');
   } else {
     fillForm.classList.add('collapsed');
+    fillArrow.classList.remove('arrowUp');
   }
 }
+// function arrowRotate() {
+//   if(designForm.classList.add('collapsed');)
+//   fillArrow.classList.add('arrowUp');
+//   }
+
+// }
 
 //eventos
 designUnfold.addEventListener('click', handleClickDesignUnfold);
