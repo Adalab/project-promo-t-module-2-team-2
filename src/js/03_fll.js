fe82e433b1147ce4cb249d1de255e503edc82b7d
MartaBelmonte <martabelmonts5@gmail.com>
2023-05-26 15:26:59 +0200

    links js

--- src/js/03_fll.js
+++ src/js/03_fll.js
@@ -5,6 +5,12 @@ const inputName = document.querySelector ('.js-name');
 const previewName = document.querySelector ('.js-preview-name');
 const inputJob = document.querySelector ('.js-job');
 const previewJob = document.querySelector ('.js-preview-job');
+const inputMail = document.querySelector ('.js-mail');
+const previewMail = document.querySelector ('.js_mail');
+const inputLinkedin = document.querySelector ('.js-linkedin');
+const previewLinkedin = document.querySelector ('.js_linkedin');
+const inputGithub = document.querySelector ('.js-github');
+const previewGithub = document.querySelector ('.js_github');
 
 
 //funciones
@@ -18,7 +24,24 @@ function handlerInputJob (event){
   previewJob.innerHTML = inputValue;
 }
 
+function handlerInputMail (event) {
+  const inputValue = event.target.value;
+  previewMail.href = inputValue; 
+}
+
+function handlerInputLinkedin (event) {
+  const inputValue = event.target.value;
+  previewLinkedin.href = inputValue; 
+}
+
+function handlerInputGithub (event) {
+  const inputValue = event.target.value;
+  previewGithub.href = inputValue; 
+}
 
 //eventos
 inputName.addEventListener('input', handlerInputName);
 inputJob.addEventListener('input', handlerInputJob);
+inputMail.addEventListener('input', handlerInputMail);
+inputLinkedin.addEventListener('input', handlerInputLinkedin);
+inputGithub.addEventListener('input', handlerInputGithub);
